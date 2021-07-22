a376fb78f825ebb0be4a7a1da406ffaab68e1cbc
Jonathan Hyland <85582235+jonathan-hyland@users.noreply.github.com>
2021-07-22 13:20:16 -0400

    Completed workspace example from Ch14

---
+++ workspace_example/adder/src/main.rs
@@ -0,0 +1,17 @@
+use add_one;
+use add_two;
+use rand;
+
+fn main() {
+    let num = 10;
+    println!(
+        "Hello, world! {} plus one is {}!",
+        num,
+        add_one::add_one(num)
+    );
+    println!(
+        "Hello, world! {} plus two is {}!",
+        num,
+        add_two::add_two(num)
+    );
+}
